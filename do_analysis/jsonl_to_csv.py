3ef11d72d8a859fa01ba3f691a8ffb2200d1abd4
Harish Thuwal <hthuwal@gmail.com>
2018-07-31 23:54:04 +0530

    Script to convert JSON to csv file to do manual analysis

---
+++ do_analysis/jsonl_to_csv.py
@@ -0,0 +1,46 @@
+import json
+import csv
+
+count = 0
+with open("ARC-Challenge-Test_qapredictions_dgem_default.jsonl", "r") as in_file, open("out.csv", "w") as out_file:
+    csvw = csv.writer(out_file)
+    for line in in_file:
+        line = json.loads(line)
+        question = line['question']['stem']
+        correct_answer = line['answerKey']
+        selected_answer = line['selected_answers']
+        options_text = {}
+        options_support = {}
+
+        for choice in line['question']['choices']:
+            label = choice['label']
+            if label not in options_text:
+                options_text[label] = choice['text']
+            if label not in options_support:
+                options_support[label] = ""
+            if choice['support'] != "":
+                options_support[label] += (choice['support']['text'] + "||")
+
+        final = {}
+        final['Question'] = question
+        final['A'] = options_text['A'] if 'A' in options_text else ""
+        final['B'] = options_text['B'] if 'B' in options_text else ""
+        final['C'] = options_text['C'] if 'C' in options_text else ""
+        final['D'] = options_text['D'] if 'D' in options_text else ""
+        final['E'] = options_text['E'] if 'E' in options_text else ""
+
+        final['support_A'] = options_support['A'] if 'A' in options_text else ""
+        final['support_B'] = options_support['B'] if 'B' in options_text else ""
+        final['support_C'] = options_support['C'] if 'C' in options_text else ""
+        final['support_D'] = options_support['D'] if 'D' in options_text else ""
+        final['support_E'] = options_support['E'] if 'E' in options_text else ""
+
+        final['correct_answer'] = correct_answer
+        final['predicted_answers'] = selected_answer
+        final['score'] = line['question_score']
+
+        if count == 0:
+            csvw.writerow(final.keys())
+        count += 1
+        csvw.writerow(final.values())
+        print("\r%d" % count, end="")
