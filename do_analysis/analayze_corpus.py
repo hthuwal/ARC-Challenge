8a9a23e586e56e3f3dded3b4006c14fe4d7d94bd
Harish Thuwal <hthuwal@gmail.com>
2018-08-14 17:22:09 +0530

    Script to analyze a corpus

---
+++ do_analysis/analayze_corpus.py
@@ -0,0 +1,32 @@
+import sys
+import os
+import csv
+from tqdm import tqdm
+
+corpus_name, _ = os.path.splitext(os.path.basename(sys.argv[1]))
+print(corpus_name)
+
+corpus_file = open(sys.argv[1], "r")
+entities_file = open("named_entities.txt", "r")
+
+entities = {word: 0 for word in entities_file.read().splitlines()}
+entities_file.close()
+
+for line in tqdm(corpus_file):
+    line = line.split()
+    for word in line:
+        if word in entities:
+            entities[word] += 1
+
+out_file = open(corpus_name + "_analysis.csv", "w")
+csvw = csv.writer(out_file)
+csvw.writerow(["Word", "num_occurrences"])
+
+entities = [(key, entities[key]) for key in entities]
+entities.sort(key=lambda x: x[0])
+
+for a, b in entities:
+    csvw.writerow([a, b])
+
+corpus_file.close()
+out_file.close()
