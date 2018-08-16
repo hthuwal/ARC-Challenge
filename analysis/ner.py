1e417a05a036e2155cabb62e220ef5273bba4498
Harish Thuwal <hthuwal@gmail.com>
2018-08-16 23:13:06 +0530

    relative paths, better cleaning

--- analysis/ner.py
+++ analysis/ner.py
@@ -1,23 +1,22 @@
-from nltk.tag import StanfordNERTagger, StanfordPOSTagger
+from nltk.tag import StanfordNERTagger
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import json
 from tqdm import tqdm
 import re
 
-sp = StanfordPOSTagger('/home/harish/dell_storage/MTP/stanford-postagger-full-2018-02-27/models/english-bidirectional-distsim.tagger',
-                       '/home/harish/dell_storage/MTP/stanford-ner-2018-02-27/stanford-ner-3.9.1.jar',
-                       encoding='utf-8')
+# sp = StanfordPOSTagger('../bin/stanford-postagger-full-2018-02-27/models/english-bidirectional-distsim.tagger',
+#                        '../bin/stanford-ner-2018-02-27/stanford-ner-3.9.1.jar',
+#                        encoding='utf-8')
 
-class3 = '/home/harish/dell_storage/MTP/stanford-ner-2018-02-27/classifiers/english.all.3class.distsim.crf.ser.gz'
-class7 = '/home/harish/dell_storage/MTP/stanford-ner-2018-02-27/classifiers/english.muc.7class.distsim.crf.ser.gz'
+class3 = '../bin/stanford-ner-2018-02-27/classifiers/english.all.3class.distsim.crf.ser.gz'
+class7 = '../bin/stanford-ner-2018-02-27/classifiers/english.muc.7class.distsim.crf.ser.gz'
 
 st = StanfordNERTagger(class7,
-                       '/home/harish/dell_storage/MTP/stanford-ner-2018-02-27/stanford-ner-3.9.1.jar',
+                       '../bin/stanford-ner-2018-02-27/stanford-ner-3.9.1.jar',
                        encoding='utf-8')
 stop_words = set(stopwords.words('english')) | set([".", ","])
 regex = re.compile('[^a-zA-Z]')
-
 count = 0
 
 
@@ -53,7 +52,8 @@ with open("ARC-Challenge-Test_qapredictions_dgem_default.jsonl", "r") as in_file
             tokenized_text = word_tokenize(text.lower())
             data = data + tokenized_text
 
-    cleaned_data = [regex.sub('', token) for token in data if token not in stop_words]
+    cleaned_data = [regex.sub('', token) for token in data]
+    cleaned_data = [token for token in cleaned_data if token not in stop_words]
     classified_data = st.tag(cleaned_data)
     classified_data = set([token[0] for token in classified_data])
 
