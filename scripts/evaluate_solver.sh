524cd57c968803e6077f88315e38de697d8fcacc
Harish Thuwal <hthuwal@gmail.com>
2018-08-27 13:26:38 +0530

    Nomore debugging

--- scripts/evaluate_solver.sh
+++ scripts/evaluate_solver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-set -x
-trap read debug
+# set -x
+# trap read debug
 # ----------------------------------------------------------------
 # Evaluate an entailment model (DGEM/DecompAtt) on the QA dataset
 # ----------------------------------------------------------------
