ec046a05a9213d1a659fe7f88b78febd98e3c2ec
Harish Thuwal <hthuwal@gmail.com>
2018-09-12 18:02:37 +0530

    Add verbosity

--- openie/stanford/stanford-openie.sh
+++ openie/stanford/stanford-openie.sh
@@ -16,6 +16,8 @@ TMP_DIR=/tmp/openie/large_corpus
 rm -rf $TMP_DIR
 mkdir -p $TMP_DIR
 
+echo "Splitting the data into small 1kb files"
+
 if [ "$(uname)" == "Darwin" ]; then
     gsplit -b 1k --numeric-suffixes $1 ${TMP_DIR}/small_
 else
@@ -23,9 +25,11 @@ else
 fi
 
 file_list="/tmp/openie/file_list.txt"
-
 find ${TMP_DIR}/small_* > "$file_list"
 
+num_files=$(find ${TMP_DIR}/small_* -type f | wc -l)
+
+echo "Splitted $1 into $num_files 1kb files"
 
 if ! [ -f "$2" ]; then
 	if [ "$3" == "coref" ]; then
@@ -33,7 +37,7 @@ if ! [ -f "$2" ]; then
 		java -mx30g -cp "stanford-corenlp/*" edu.stanford.nlp.naturalli.OpenIE -threads 16 -resolve_coref true -annotators tokenize,ssplit,pos,lemma,ner,depparse,natlog,coref,openie -filelist "$file_list" -format ollie > "$2"
 	else
 		echo "Without Anaphora Resolution"
-		java -mx4g -cp "stanford-corenlp/*" edu.stanford.nlp.naturalli.OpenIE -threads 16 -filelist "$file_list" -format ollie > "$2"
+		java -mx30g -cp "stanford-corenlp/*" edu.stanford.nlp.naturalli.OpenIE -threads 16 -filelist "$file_list" -format ollie > "$2"
 	fi
 else
 	echo "$2 already exists"
