e58152c9dd80fc0c301554c08094249ac5bb0224
Harish Thuwal <hthuwal@gmail.com>
2018-09-12 18:06:56 +0530

    Display time taken in splitting and by the actual java call

--- openie/stanford/stanford-openie.sh
+++ openie/stanford/stanford-openie.sh
@@ -16,12 +16,12 @@ TMP_DIR=/tmp/openie/large_corpus
 rm -rf $TMP_DIR
 mkdir -p $TMP_DIR
 
-echo "Splitting the data into small 1kb files"
+echo -e "Splitting the data into small 1kb files\n"
 
 if [ "$(uname)" == "Darwin" ]; then
     gsplit -b 1k --numeric-suffixes $1 ${TMP_DIR}/small_
 else
-    split -b 1k --numeric-suffixes $1 ${TMP_DIR}/small_
+    time split -b 1k --numeric-suffixes $1 ${TMP_DIR}/small_
 fi
 
 file_list="/tmp/openie/file_list.txt"
@@ -29,15 +29,15 @@ find ${TMP_DIR}/small_* > "$file_list"
 
 num_files=$(find ${TMP_DIR}/small_* -type f | wc -l)
 
-echo "Splitted $1 into $num_files 1kb files"
+echo "Splitted $1 into $num_files 1kb files\n"
 
 if ! [ -f "$2" ]; then
 	if [ "$3" == "coref" ]; then
 		echo "With Anaphora Resolution"
-		java -mx30g -cp "stanford-corenlp/*" edu.stanford.nlp.naturalli.OpenIE -threads 16 -resolve_coref true -annotators tokenize,ssplit,pos,lemma,ner,depparse,natlog,coref,openie -filelist "$file_list" -format ollie > "$2"
+		time java -mx30g -cp "stanford-corenlp/*" edu.stanford.nlp.naturalli.OpenIE -threads 16 -resolve_coref true -annotators tokenize,ssplit,pos,lemma,ner,depparse,natlog,coref,openie -filelist "$file_list" -format ollie > "$2"
 	else
 		echo "Without Anaphora Resolution"
-		java -mx30g -cp "stanford-corenlp/*" edu.stanford.nlp.naturalli.OpenIE -threads 16 -filelist "$file_list" -format ollie > "$2"
+		time java -mx30g -cp "stanford-corenlp/*" edu.stanford.nlp.naturalli.OpenIE -threads 16 -filelist "$file_list" -format ollie > "$2"
 	fi
 else
 	echo "$2 already exists"
