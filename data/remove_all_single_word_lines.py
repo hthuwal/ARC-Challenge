d37f04670db71d90fae445cf4b512a7d263c87b9
Harish Thuwal <hthuwal@gmail.com>
2018-10-01 18:40:03 +0530

    Script to clean data to run it on Mausam's openIE

---
+++ data/remove_all_single_word_lines.py
@@ -0,0 +1,18 @@
+import sys
+import os
+
+file = os.path.abspath(sys.argv[1])
+name, ext = os.path.splitext(file)
+target_file = name + "_dair" + ext
+ends = ['?', '.', '!']
+
+with open(file, "r") as f, open(target_file, "w") as out:
+    count = 0
+    for line in f:
+        print("\r" + str(count), end=" ")
+        if len(line.strip().split()) > 1:
+            if line[-1] not in ends:
+                line = line + "."
+                out.write(line + "\n")
+        count += 1
+    print("\nDone\n")
