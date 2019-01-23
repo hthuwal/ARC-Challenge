b0cf421af76619a9dac60d1f3e1a8b7b0a0a3c65
Harish Thuwal <hthuwal@gmail.com>
2019-01-23 15:04:28 +0530

    Don't Ignore options with option labels 1,2,3,4

--- openie/stanford/create_graph_for_qa.py
+++ openie/stanford/create_graph_for_qa.py
@@ -1,6 +1,5 @@
 import json
 import utils
-from graph import Graph
 from tqdm import tqdm
 import sys
 import dill as pickle
@@ -8,25 +7,19 @@ q_graphs = {}
 out = sys.argv[1]
 coref = sys.argv[2]
 coref = True if coref == "True" else False
+
 with open("../../data/ARC-V1-Feb2018-2/ARC-Challenge/ARC-Challenge-Test.jsonl", "r") as in_file:
     lines = in_file.readlines()
     for line in tqdm(lines, ascii=True):
         line = json.loads(line.strip())
         question = line['question']['stem']
         correct_answer = line['answerKey']
-        options_text = {}
+        options = {}
 
         for choice in line['question']['choices']:
             label = choice['label']
-            if label not in options_text:
-                options_text[label] = choice['text']
-
-        options = {}
-        options['A'] = options_text['A'] if 'A' in options_text else ""
-        options['B'] = options_text['B'] if 'B' in options_text else ""
-        options['C'] = options_text['C'] if 'C' in options_text else ""
-        options['D'] = options_text['D'] if 'D' in options_text else ""
-        options['E'] = options_text['E'] if 'E' in options_text else ""
+            if label not in options:
+                options[label] = choice['text']
 
         hypothesis = {}
         question = utils.replace_wh_word_with_blank(question)
