92bf22c47bb23526455b3ecdbab1d10ca46e090a
Harish Thuwal <hthuwal@gmail.com>
2019-04-15 23:54:42 +0530

    Automatically Cleanup and Preprocess before running

--- stuffie/run.sh
+++ stuffie/run.sh
@@ -1,5 +1,8 @@
-rm parts/*.openie
-rm parts/*.exceptions
+rm -rf parts
+mkdir parts
+
+echo "Splitting Source file into $1 components..."
+time split -n $1 --numeric-suffixes "../data/ARC-V1-Feb2018-2/ARC_Corpus.txt" parts/part_
 
 echo "Compiling.."
 
@@ -8,7 +11,7 @@ cstatus=$?
 echo "Running"
 
 if [ $cstatus -eq 0 ]; then
-    mvn exec:java -Dexec.mainClass=it.unibz.inf.stuffie.StuffieConsoleRunner
+    mvn -X exec:java -Dexec.mainClass=it.unibz.inf.stuffie.StuffieConsoleRunner
 else
     echo "Compilation Failed.."
 fi
