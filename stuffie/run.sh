e8ba251d7fc30ae1b8408586f87338bd8986b7bc
Harish Thuwal <hthuwal@gmail.com>
2019-04-18 22:45:24 +0530

    Make file UTF-8 parsable

--- stuffie/run.sh
+++ stuffie/run.sh
@@ -1,11 +1,27 @@
 if [[ $2 == 'reset' ]]
 then
-	rm -rf parts
+	rm -rf parts completed
+
+    mkdir -p completed/parts
 	mkdir parts
+    
     echo "Splitting Source file into $1 components..."
     time split -n $1 --numeric-suffixes "../data/ARC-V1-Feb2018-2/ARC_Corpus.txt" parts/part_
+
+    echo "Cleaning Parts according to UTF-8"
+    cd parts
+
+    for each in *
+    do
+    	printf "\r$each"
+    	iconv -f utf-8 -t utf-8 -c "$each" > temp 2> /dev/null
+    	mv temp "$each"
+    done
+    cd ..
 fi
 
+rm parts/*.openie
+rm parts/*.exceptions
 
 echo "Compiling.."
 
