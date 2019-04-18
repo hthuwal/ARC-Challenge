a90613c259f95306506b5f2ec8f4b59705fe5a7f
Harish Thuwal <hthuwal@gmail.com>
2019-04-18 15:46:29 +0530

    Rename and Use File Read Backwards Library

--- stuffie/list-incompleted-files.py
+++ stuffie/list-incompleted-files.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from tqdm import tqdm
+from file_read_backwards import FileReadBackwards
 
 def preprocess(line):
 	line = line.strip()
@@ -12,43 +13,39 @@ if __name__ == '__main__':
 	target_dir = sys.argv[1]
 
 	incomplete_files = []
-	for i in tqdm(range(10000)):
+	for i in tqdm(range(10000), ascii=True):
 		i = format(i, '04d')
 		file = os.path.join(target_dir, f"part_{i}")
 		openie = file + ".openie"
 		exceptions = file + ".exceptions"
-
 		# print(f"Checking file: {file}...")
 		
-		lines = [each.strip() for each in open(file).readlines()]
-		lines = [each for each in lines if each]
-		last_line = preprocess(lines[-1].strip())
+		for each in FileReadBackwards(file):
+			each = each.strip()
+			if each:
+				last_line = preprocess(each)
+				break
 		# print(f"last_line: {last_line}")
-		del lines
 
 		if os.path.exists(openie):
-			lines = open(openie).readlines()
 			last_parsed_line = None
-
-			for i in range(-1, -len(lines)-1, -1):
-				line = lines[i]
+			for line in FileReadBackwards(openie):
 				if line.startswith("##"):
 					last_parsed_line = line[2:].strip()
 					# print(f"last_parsed_line: {last_parsed_line}")
 					break
-
-			del lines
 
 			if last_parsed_line and last_parsed_line == last_line:
 				# print(file, "is complete.")
 				continue
 
 		if os.path.exists(exceptions):
-			lines = [each.strip() for each in open(exceptions).readlines()]
-			lines = [each for each in lines if each]
-			last_error_line = lines[-1].strip()
-			# print(f"last_error_line: {last_error_line}")
-			del lines
+			last_error_line = None
+			for each in FileReadBackwards(file):
+				each = each.strip()
+				if each:
+					last_error_line = each
+					break
 
 			if last_error_line == last_line:
 				# print(file, "is complete")
@@ -59,5 +56,6 @@ if __name__ == '__main__':
 			incomplete_files.append(file)
 			# print(file, "is not complete")
 	
+	print("incomplete_files..")
 	for file in incomplete_files:
 		print(file)
