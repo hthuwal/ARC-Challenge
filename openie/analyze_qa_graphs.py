6d764da82ace46127af1d331214622a770b9c8ce
Harish Thuwal <hthuwal@gmail.com>
2019-05-20 06:00:21 +0530

    Script to analyze qa_graphs

---
+++ openie/analyze_qa_graphs.py
@@ -0,0 +1,36 @@
+import dill as pickle
+import click
+
+from collections import defaultdict
+
+
+def get_qa_graph(path):
+    """
+    Load the qa graph pickle dump located @PATH
+    """
+    print(f"Loading QA Dump @ {path}")
+    return pickle.load(open(path, "rb"))
+
+
+def analyze(qa_graphs):
+    size = defaultdict(int)
+    for qid in qa_graphs:
+        hypo_graphs = qa_graphs[qid]['hypothesis_graphs']
+        for option in hypo_graphs:
+            hypo_graph = hypo_graphs[option]
+            size[hypo_graph.num_nodes()] += 1
+
+    size = list(size.items())
+    size.sort(key=lambda x: x[0])
+    print(size)
+
+
+@click.command()
+@click.argument("qa_graph_path", type=click.Path(exists=True))
+def main(qa_graph_path):
+    qa_graphs = get_qa_graph(qa_graph_path)
+    analyze(qa_graphs)
+
+
+if __name__ == '__main__':
+    main()
